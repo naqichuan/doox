f66b4778ca580b63fe3000d03bb4a11385fa7b32
Naqi Chuan <naqichuan@163.com>
2019-12-20 09:20:44 +0800

    M

---
+++ commons/commons-web/src/main/java/org/nqcx/doox/commons/web/filter/CROSFilter.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 nqcx.org All right reserved. This software is the confidential and proprietary information
+ * of nqcx.org ("Confidential Information"). You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you entered into with nqcx.org.
+ */
+
+package org.nqcx.doox.commons.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xiaola on 19/1/20.
+ */
+public class CROSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        // ignore
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "S_ID,content-type");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        // ignore
+    }
+}
+
